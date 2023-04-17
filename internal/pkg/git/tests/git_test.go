86c24af4215d7d4697d6feb14997d5332d55d633
blackmarllbor0 <3100194@gmail.com>
2023-04-17 21:20:20 +0300

    optimized the writing of tests and put layouts in the right place, so you can reuse them and not get tangled up in packages

--- internal/pkg/git/tests/git_test.go
+++ internal/pkg/git/tests/git_test.go
@@ -2,75 +2,68 @@ package tests
 
 import (
 	"fmt"
-	"github.com/blackmarllboro/create-project-struct/internal/pkg/git"
-	"github.com/blackmarllboro/create-project-struct/internal/pkg/git/tests/mock"
 	"os"
 	"testing"
+
+	"github.com/blackmarllboro/create-project-struct/internal/pkg/args"
+	"github.com/blackmarllboro/create-project-struct/internal/pkg/args/tests/mocks"
+	"github.com/blackmarllboro/create-project-struct/internal/pkg/git"
 )
 
-// Since the tests in Go are run in parallel, there is a problem with creating
-// dirs. So i made one of the tests a simple function, and then they run fine.
-func testCreateLocalGitRepositoryNewdir(t *testing.T) {
-	t.Log("create dir for test")
-	{
-		if err := os.Mkdir(mock.TestDir, 0755); err != nil {
-			t.Fatalf("Failed to create test directory: %v", err)
-		}
+func TestCreateLocalGitRepository(t *testing.T) {
+	const currentDir = "current_dir"
+
+	data := []struct {
+		name string
+		mock args.GetProjectName
+	}{
+		{
+			name: currentDir,
+			mock: mocks.IsCurrentDirIsTrue{},
+		},
+		{
+			name: mocks.ProjName,
+			mock: mocks.IsCurrentDirIsFalse{},
+		},
 	}
 
-	t.Log("create repository in new dir")
-	{
-		if err := git.CreateLocalGitRepository(mock.IsCurrentDirIsFalse{}); err != nil {
-			t.Fatalf("Expected no error, but got: %s", err)
-		}
-	}
+	for _, dataT := range data {
+		{
+			t.Log("create dir for ", dataT.name)
+			{
+				if err := os.Mkdir(dataT.name, 0755); err != nil {
+					t.Fatalf("Failed to create test directory: %v", err)
+				}
 
-	t.Log("check the .git in the current directory")
-	{
-		_, err := os.Stat(".git")
-		if os.IsNotExist(err) {
-			t.Fatal(".git directory not found in the expected location")
-		}
-	}
+				if dataT.name == currentDir {
+					if err := os.Chdir(fmt.Sprintf("./%s", currentDir)); err != nil {
+						t.Fatalf("Expected no error, but got: %s", err)
+					}
+				}
+			}
 
-	t.Log("SUCCESS")
-}
+			t.Log("create repository in dir: ", dataT.name)
+			{
+				if err := git.CreateLocalGitRepository(dataT.mock); err != nil {
+					t.Fatalf("Expected no error, but got: %s", err)
+				}
+			}
 
-func TestCreateLocalGitRepository_CurrentDir(t *testing.T) {
-	testCreateLocalGitRepositoryNewdir(t)
+			t.Log("check the .git in the directory: ", dataT.name)
+			{
+				_, err := os.Stat(".git")
+				if os.IsNotExist(err) {
+					t.Fatal(".git directory not found in the expected location")
+				}
+			}
 
-	t.Log("create dir for test")
-	{
-		if err := os.Mkdir(mock.TestDir, 0755); err != nil {
-			t.Fatalf("Failed to create test directory: %v", err)
-		}
-		defer os.RemoveAll(fmt.Sprintf("../%s", mock.TestDir))
-
-		if err := os.Chdir(mock.TestDir); err != nil {
-			t.Fatalf("Expected no error, but got: %s", err)
-		}
-
-		defer func() {
 			if err := os.Chdir(".."); err != nil {
 				t.Fatalf("Expected no error, but got: %s", err)
 			}
-		}()
-	}
 
-	t.Log("create repository in current dir")
-	{
-		if err := git.CreateLocalGitRepository(mock.IsCurrentDirIsTrue{}); err != nil {
-			t.Fatalf("Expected no error, but got: %s", err)
+			os.RemoveAll(fmt.Sprintf("./%s", dataT.name))
+
+			t.Log("SUCCESS")
 		}
 	}
-
-	t.Log("check the .git in the current directory")
-	{
-		_, err := os.Stat(".git")
-		if os.IsNotExist(err) {
-			t.Fatal(".git directory not found in the expected location")
-		}
-	}
-
-	t.Log("SUCCESS")
 }
